6cd54b4f21ea9beefb4c886457147e587e57f3af
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-08-03 16:00:28 +0200

    home page

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import Header from './components/Header'
 import { Home } from "./pages/home"
 import { Categories } from "./pages/categories"
 import { Basket } from "./pages/basket"
+import { useEffect, useState } from "react"
 
 
 function App() {
+
   return (
     <>
       <Header />
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,33 @@
-export function Home(){
-    return(
-        <h1>this is home!</h1>
-    )
+import { useEffect, useState } from "react";
+
+type Product = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  categoryId: number;
+  image: string;
+};
+
+export function Home() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/products")
+      .then((resp) => resp.json())
+      .then((productsFromServer) => setProducts(productsFromServer));
+  }, []);
+
+  return (
+    <div className="products-container">
+      <ul className="products-container__list">
+        {products.map((item) => (
+          <li className="product-item">
+            <img src={item.image}></img>
+            <h3>{item.title}</h3>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
